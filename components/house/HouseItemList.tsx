f5a4dbe825c686ca7a21a228253dfe462442fbbf
Ethirajulu <ethirajulusukumar@gmail.com>
2020-11-15 19:17:17 +0530

    House list refresh alignment issue fixed

--- components/house/HouseItemList.tsx
+++ components/house/HouseItemList.tsx
@@ -17,7 +17,8 @@ import { setLoading } from "../../redux/actions";
 import { deleteHouseThunk, markNotPaidThunk } from "../../redux/thunk";
 import { EDIT_LABEL, House } from "../../redux/types";
 
-import styles from "../../styles/ListItem.module.css";
+// import styles from "../../styles/ListItem.module.css";
+import styles from "../../styles/HouseItem.module.css";
 import { checkIsPaid } from "../../utils";
 
 export interface HouseItemListProps {
@@ -92,85 +93,67 @@ const HouseItemList: FC<HouseItemListProps> = ({
       itemLayout="vertical"
       dataSource={houses}
       renderItem={(house) => (
-        <Item
-          key={house.id}
-          extra={
-            <div className={styles.pay_status}>
-              {house.last_paid && checkIsPaid(house.last_paid) ? (
-                <Button
-                  type="link"
-                  onClick={() => onPaidClick(house)}
-                  style={{ color: "green" }}
-                >
-                  <CheckOutlined /> Paid
-                </Button>
-              ) : (
-                <Button
-                  type="link"
-                  danger
-                  onClick={() => onNotPaidClick(house)}
-                >
-                  Not Paid
-                </Button>
-              )}
-            </div>
-          }
-          className={styles.item}
-        >
-          <Item.Meta
-            avatar={<Avatar src="/images/house.svg" />}
-            title={house.name}
-            description={
-              <Row>
-                <Col span={24} className={styles.des_col}>
-                  <Space split={<Divider type="vertical" />}>
-                    <IconText
-                      icon={DollarCircleOutlined}
-                      text={house.default_amt}
-                      key="box_no"
-                    />
-                    <IconText
-                      icon={FieldNumberOutlined}
-                      text={house.box_no}
-                      key="amount"
-                    />
-                  </Space>
-                </Col>
-                <Col span={24}>
-                  <Row>
-                    <Col xs={{ offset: 18 }} md={{ offset: 23 }}>
-                      <Space size="small" split={<Divider type="vertical" />}>
-                        <EditOutlined
-                          onClick={() => onEditClick(house)}
-                          className={styles.edit_button}
-                        />
-                        <DeleteOutlined
-                          onClick={() => onDeleteClick(house)}
-                          className={styles.delete_button}
-                        />
-                        <Link
-                          href={{
-                            pathname: `/logs`,
-                            query: {
-                              ...house,
-                              areaName,
-                            },
-                          }}
-                          passHref
-                        >
-                          <a>
-                            <Button icon={<InfoCircleOutlined />} />
-                          </a>
-                        </Link>
-                      </Space>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            }
-            className={styles.meta}
-          />
-        </Item>
+        <div className={styles.house_grid}>
+          <div className={styles.avatar_grid}>
+            <Avatar src="/images/house.svg" />
+          </div>
+          <h4 className={styles.name_grid}>{house.name}</h4>
+          <div className={styles.status_grid}>
+            {house.last_paid && checkIsPaid(house.last_paid) ? (
+              <Button
+                type="link"
+                onClick={() => onPaidClick(house)}
+                style={{ color: "green" }}
+              >
+                <CheckOutlined /> Paid
+              </Button>
+            ) : (
+              <Button type="link" danger onClick={() => onNotPaidClick(house)}>
+                Not Paid
+              </Button>
+            )}
+          </div>
+          <div className={styles.details_grid}>
+            <Space split={<Divider type="vertical" />}>
+              <IconText
+                icon={DollarCircleOutlined}
+                text={house.default_amt}
+                key="box_no"
+              />
+              <IconText
+                icon={FieldNumberOutlined}
+                text={house.box_no}
+                key="amount"
+              />
+            </Space>
+          </div>
+          <div className={styles.actions_grid}>
+            <Space size="small" split={<Divider type="vertical" />}>
+              <EditOutlined
+                onClick={() => onEditClick(house)}
+                className={styles.edit_button}
+              />
+              <DeleteOutlined
+                onClick={() => onDeleteClick(house)}
+                className={styles.delete_button}
+              />
+              <Link
+                href={{
+                  pathname: `/logs`,
+                  query: {
+                    ...house,
+                    areaName,
+                  },
+                }}
+                passHref
+              >
+                <a>
+                  <Button icon={<InfoCircleOutlined />} />
+                </a>
+              </Link>
+            </Space>
+          </div>
+        </div>
       )}
     />
   );
